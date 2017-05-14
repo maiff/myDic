4b9f174e4ff8ba16ff5ec2d731a8f19aac2a16f0
mmx <864306867@qq.com>
2017-05-15 01:38:15 +0800

    add look up

--- app.js
+++ app.js
@@ -13,6 +13,9 @@ app.use('/login', login)
 const add = require('./router/add')
 app.use('/add', add)
 
+const lookup = require('./router/lookup')
+app.use('/lookup', lookup)
+
 app.listen(port, () => {
   console.log(`listen on ${port}`)
 })
---
+++ router/lookup.js
@@ -0,0 +1,34 @@
+let router = require('../lib/MyExpress')()
+const getProxy = require('../lib/getProxy')
+const query = require('../lib/query')
+const sendJson = require('../lib/sendJson')
+router.use(query())
+router.use(sendJson())
+var parseString = require('xml2js').parseString
+
+router.get((req, res) => {
+  let q = req.query.q
+  // console.log(req.body)
+  getProxy({
+    query: {
+      q: q
+    },
+    url: 'http://dict.youdao.com/fsearch?q=vue'
+  }).then((text) => {
+    return new Promise(function (resolve, reject) {
+      parseString(text, function (err, result) {
+        err && reject(err)
+        resolve(result)
+      })
+    })
+  }, (err) => {
+    console.log(err)
+  }).then((obj) => {
+        // console.log(obj)
+    res.json(obj)
+  }, (err) => {
+    console.log(err)
+  })
+})
+
+module.exports = router
